6cdcea3dc547ca15deabd1bae5a2c83a5ce811ee
Chris Athanas <realityexpanderdev@gmail.com>
2018-11-29 09:39:26 -0600

    Update militaryTime.js

--- militaryTime.js
+++ militaryTime.js
@@ -3,10 +3,11 @@ function toMilitary (time) {
   hours = time.split(":")[0];
   mins = time.split(":")[1].slice(0,2);
   
-  if(time.includes("pm")) {
+  if(time.includes("pm") && hours < 12) {
     hours = parseInt(hours) + 12;
-    if (hours>23)
-      hours = 0;
+  }
+  if(time.includes("am") && hours > 11) {
+    hours = 0;
   }
   
   if ( (hours+"").length === 1)
